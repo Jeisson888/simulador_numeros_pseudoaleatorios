99f092c6987edaf2ab80cec2628ccc437f408f73
jeisson <jeissontorres888@gmail.com>
2025-03-04 18:06:34 -0500

    cuadrados_medios

--- modelo/generar.py
+++ modelo/generar.py
@@ -12,9 +12,17 @@ def congruencial_lineal(iteraciones, multiplicador, semilla, corrimiento, modulo
 
     return resultados
 
-def cuadrados_medios():
-    # completar
-    pass
+def cuadrados_medios(iteraciones, semilla):
+    resultados = []
+
+    for i in range(iteraciones):
+        semilla2 = semilla ** 2
+        cadena = str(semilla2)
+        digitos_centrales = cadena[(len(cadena) - 4) // 2 : ((len(cadena) - 4) // 2) + 4]
+        resultados.append(int(digitos_centrales))
+        semilla = int(digitos_centrales)
+
+    return resultados
 
 def otro_algoritmo():
     # completar
--- presentador/main.py
+++ presentador/main.py
@@ -2,7 +2,7 @@ import sys
 import os
 sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
 
-from modelo import generar
+from modelo.generar import *
 
 iteraciones = 20
 multiplicador = 5
@@ -10,5 +10,9 @@ semilla = 7
 corrimiento = 3
 modulo = 16
 
-resultados = generar.congruencial_lineal(iteraciones, multiplicador, semilla, corrimiento, modulo)
+resultados = congruencial_lineal(iteraciones, multiplicador, semilla, corrimiento, modulo)
+print(resultados)
+
+semilla = 2573
+resultados = cuadrados_medios(iteraciones, semilla)
 print(resultados)
