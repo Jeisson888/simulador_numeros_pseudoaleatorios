96a7bfd3ef1b5dbf4ed0d53b2cc5d8b61d9544b6
jeisson <jeissontorres888@gmail.com>
2025-03-04 22:50:42 -0500

    mejoras chi2

--- modelo/probar.py
+++ modelo/probar.py
@@ -2,20 +2,19 @@ import scipy.stats as stats
 import math
 import numpy as np
 
-def chi_cuadrada(numeros):
+def chi_cuadrada(numeros, desde, hasta):
     n = len(numeros)
     m = math.ceil(math.sqrt(n))
-    minimo, maximo = min(numeros), max(numeros)
-    intervalos = np.linspace(minimo, maximo, m + 1)
+    intervalos = np.linspace(desde, hasta, m + 1)
 
     frecuencias_observadas, _ = np.histogram(numeros, bins=intervalos)
     frecuencia_esperada = n / m  
 
     chi2 = sum((o - frecuencia_esperada) ** 2 / frecuencia_esperada for o in frecuencias_observadas)
     grados_libertad = m - 1
-    p_valor = 1 - stats.chi2.cdf(chi2, grados_libertad)
+    chi2_tabla = stats.chi2.ppf(1 - 0.05, grados_libertad)
     
-    return chi2, p_valor
+    return chi2, chi2_tabla
 
 def kolmogorov_smirnov():
     # completar
